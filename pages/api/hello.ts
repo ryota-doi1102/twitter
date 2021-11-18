bcfab0c20385d48712a7f85d0defb5d01fdd38e9
Shalin Okada <shalin.okada@wnd.co.jp>
2021-11-18 18:39:53 +0900

    Remove api folder

--- pages/api/hello.ts
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-const hello = (req: NextApiRequest, res: NextApiResponse): void => {
-  res.statusCode = 200;
-  res.json({ name: 'John Doe' });
-};
-
-export default hello;
